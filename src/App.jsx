bba694fffb2d542eb390b62fda3f82255be3a6b0
Massa-Albani4 <massa.albani0213@gmail.com>
2024-12-16 20:34:09 +0400

    Add spacing styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Chart from "./Chart";
 
 function App() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-biege-200 font-body text-brown">
-      <div className="flex flex-row bg-red text-biege-100">
+    <div className="flex h-screen flex-col items-center bg-biege-200 px-4 font-body text-brown">
+      <div className="mt-16 flex w-full flex-row gap-[9.3rem] rounded-lg bg-red px-5 py-4 text-biege-100">
         <div>
-          <p className="text-sm">My balance</p>
-          <p className="text-lg font-bold">$921.48</p>
+          <p className="text-sm font-normal">My balance</p>
+          <p className="text-[1.39rem] font-bold">$921.48</p>
         </div>
         <div>
           <svg
@@ -24,16 +24,16 @@ function App() {
           </svg>
         </div>
       </div>
-      <div className="bg-biege-100">
-        <h1 className="text-xl font-bold">Spending - Last 7 days</h1>
+      <div className="mt-4 w-full rounded-lg bg-biege-100 p-5">
+        <h1 className="mb-9 text-xl font-bold">Spending - Last 7 days</h1>
         <Chart />
-        <div className="flex flex-row border-t-2 border-biege-200">
-          <div>
+        <div className="mt-3 flex flex-row border-t-2 border-biege-200 pt-5">
+          <div className="mr-[5.5rem]">
             <p className="text-sm text-gray">Total this month</p>
-            <p className="text-xl font-bold">$478.33</p>
+            <p className="text-2xl font-bold">$478.33</p>
           </div>
-          <div>
-            <p className="text-sm font-bold">+2.4%</p>
+          <div className="ml-[.45rem] mt-3">
+            <p className="ml-[3.8rem] text-sm font-bold">+2.4%</p>
             <p className="text-sm text-gray">from last month</p>
           </div>
         </div>
